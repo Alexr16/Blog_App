43f13829e6c38a4c1745c2ca3bcaa5474a992b47
Alexr16 <j.alex-1996@hotmail.com>
2022-07-06 15:25:33 -0500

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-    belongs_to :post
-    belongs_to :author, class_name: 'User'
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
 
-    def update_posts_counter
-        post.update(comments_counter: post.comments.count)
-    end
+  def update_posts_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
-    def update_posts_counter
-        author.update(post_counter: author.post.count)
-    end
+  def update_posts_counter
+    author.update(post_counter: author.post.count)
+  end
 
-    def last_five_comments
-        comments.order(created_at: :desc).limit(5)
-    end
+  def last_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
