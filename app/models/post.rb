ae6766a7fdf9866f29fa915f8f5a4b59ae57c54c
Alexr16 <j.alex-1996@hotmail.com>
2022-07-07 11:33:22 -0500

    Add tests for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,19 +2,19 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  validate :title, presence: true, length: { maximum: 250 }
-  validate :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
 
   after_save :update_posts_counter
 
-  def update_posts_counter
-    author.update(post_counter: author.posts.count)
+  def last_five_comments
+    comments.order(created_at: :desc).limit(5)
   end
 
   private
 
-  def last_five_comments
-    comments.order(created_at: :desc).limit(5)
+  def update_posts_counter
+    author.update(post_counter: author.posts.count)
   end
 end
