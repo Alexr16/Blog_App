512fa93570afadb6e15942b78bbb23a4bb9d7ee7
Alexr16 <j.alex-1996@hotmail.com>
2022-07-07 10:25:24 -0500

    Add title length validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  validate :title, presence: true
+  validate :title, presence: true, length: { maximum: 250 }
+
 
   after_save :update_posts_counter
 
