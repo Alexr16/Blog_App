ef17cb79db923fb732e53d74c6aa189740a0675d
Alexr16 <j.alex-1996@hotmail.com>
2022-07-07 11:39:22 -0500

    Fix linter errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,56 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-    before :each do
-        @user = User.new(name: 'John Doe', photo: 'http://example.com/photo.jpg', post_counter: 0, bio: 'Teacher from Mexico.')
-        @user.save
-        @post = Post.new(author: @user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
-        @post.save
-    end
+  before :each do
+    @user = User.new(name: 'John Doe', photo: 'http://example.com/photo.jpg', post_counter: 0,
+                     bio: 'Teacher from Mexico.')
+    @user.save
+    @post = Post.new(author: @user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                     likes_counter: 0)
+    @post.save
+  end
 
-    it 'Post is valid with valid attributes' do
-        expect(@post).to be_valid
-    end
+  it 'Post is valid with valid attributes' do
+    expect(@post).to be_valid
+  end
 
-    it 'title must not be blank' do
-       @post.title = nil
-       expect(@post).to be_invalid
-    end
+  it 'title must not be blank' do
+    @post.title = nil
+    expect(@post).to be_invalid
+  end
 
-    it 'text is valid' do
-        @post.text = 'This is my first post'
-        expect(@post).to be_valid
-    end
+  it 'text is valid' do
+    @post.text = 'This is my first post'
+    expect(@post).to be_valid
+  end
 
-    it 'comments_counter must not be blank' do
-        @post.comments_counter = nil
-        expect(@post).to be_invalid
-    end
+  it 'comments_counter must not be blank' do
+    @post.comments_counter = nil
+    expect(@post).to be_invalid
+  end
 
-    it 'comments_counter must be an integer' do
-        @post.comments_counter = 'a'
-        expect(@post).to be_invalid
-    end
+  it 'comments_counter must be an integer' do
+    @post.comments_counter = 'a'
+    expect(@post).to be_invalid
+  end
 
-    it 'comment_counter must be greater than or equal to 0' do
-        @post.comments_counter = -1
-        expect(@post).to be_invalid
-    end
+  it 'comment_counter must be greater than or equal to 0' do
+    @post.comments_counter = -1
+    expect(@post).to be_invalid
+  end
 
-    it 'last_five_comments returns the last five comments' do
-        @comment = Comment.new(author: @user, post: @post, text: 'This is my first comment')
-        @comment.save
-        @comment = Comment.new(author: @user, post: @post, text: 'This is my second comment')
-        @comment.save
-        @comment = Comment.new(author: @user, post: @post, text: 'This is my third comment')
-        @comment.save
-        @comment = Comment.new(author: @user, post: @post, text: 'This is my fourth comment')
-        @comment.save
-        @comment = Comment.new(author: @user, post: @post, text: 'This is my fifth comment')
-        @comment.save
-        @comment = Comment.new(author: @user, post: @post, text: 'This is my six comment')
-        @comment.save
-        expect(@post.last_five_comments.count).to eq(5)
-        expect(@post.last_five_comments.second.text).to eq('This is my fifth comment')
-    end
+  it 'last_five_comments returns the last five comments' do
+    @comment = Comment.new(author: @user, post: @post, text: 'This is my first comment')
+    @comment.save
+    @comment = Comment.new(author: @user, post: @post, text: 'This is my second comment')
+    @comment.save
+    @comment = Comment.new(author: @user, post: @post, text: 'This is my third comment')
+    @comment.save
+    @comment = Comment.new(author: @user, post: @post, text: 'This is my fourth comment')
+    @comment.save
+    @comment = Comment.new(author: @user, post: @post, text: 'This is my fifth comment')
+    @comment.save
+    @comment = Comment.new(author: @user, post: @post, text: 'This is my six comment')
+    @comment.save
+    expect(@post.last_five_comments.count).to eq(5)
+    expect(@post.last_five_comments.second.text).to eq('This is my fifth comment')
+  end
 end
