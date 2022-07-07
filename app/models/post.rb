5ee39710f2dc5a2f8cc02652304ef44aba48cc71
Alexr16 <j.alex-1996@hotmail.com>
2022-07-07 10:24:01 -0500

    Add title validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  validate :title, presence: true
 
   after_save :update_posts_counter
 
