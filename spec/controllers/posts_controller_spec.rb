d9e1d3fc390b336f90e757d449e1a5da6c357299
Alexr16 <j.alex-1996@hotmail.com>
2022-07-11 14:38:47 -0500

    Add test for post show controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -11,9 +11,24 @@ describe 'GET #index' do
       expect(response).to render_template(:index)
     end
 
-    it 'should render the correct text in the index template' do
+    it 'should render the correct text in the show template' do
         expect(response.body).to include('This is the Posts index page.')
     end
 end
 
+describe 'GET #show' do
+    before(:each) { get user_post_path(2,1) }
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should render the correct text in the show template' do
+        expect(response.body).to include('This is the Posts show page.')
+    end
+end
 end
