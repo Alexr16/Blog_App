1bdcb289ea587704635ac81210fa735d9121caaa
Alexr16 <j.alex-1996@hotmail.com>
2022-07-12 16:25:31 -0500

    Add new, create, and post_params methods to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,23 @@ class PostsController < ApplicationController
     @user = @post.author
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      redirect_to user_post_path(current_user.id, @post.id)
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
