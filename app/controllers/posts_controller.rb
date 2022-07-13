ae62e0c6a3a729e5efd4f27914aaf9754f5deea3
Alexr16 <j.alex-1996@hotmail.com>
2022-07-13 10:50:20 -0500

    Refactor posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @user = @post.author
+    @user = User.find(params[:user_id])
+    @comment = Comment.new
     @comments = @post.comments
   end
 
@@ -23,6 +24,13 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  # def comment
+  #   @post = Post.find(params[:id])
+  #   @comment = Comments.new(post: @post.id, author: current_user.id,  text: params[:text])
+  #   @comment.save
+  #   redirect_to user_post_path(current_user.id, Post.find(params[:post_id]))
+  # end
 
   def post_params
     params.require(:post).permit(:title, :text)
