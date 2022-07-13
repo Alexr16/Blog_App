94716cd226af5b93944bda5571ce2d21298cbc2e
Alexr16 <j.alex-1996@hotmail.com>
2022-07-13 12:33:27 -0500

    Applied requested changes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,14 @@ class LikesController < ApplicationController
     @user = current_user
     @post = Post.find(params[:post_id])
     if liked?
-      redirect_to user_posts_path(@user.id)
+      redirect_to user_post_path(@user.id, @post.id)
     else
       @like = @user.likes.new
       @like.author_id = @user.id
       @like.post_id = @post.id
       @like.save
     end
-    redirect_to user_posts_path(@user.id)
+    redirect_to user_post_path(@user.id, @post.id)
   end
 
   def liked?
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,13 +25,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # def comment
-  #   @post = Post.find(params[:id])
-  #   @comment = Comments.new(post: @post.id, author: current_user.id,  text: params[:text])
-  #   @comment.save
-  #   redirect_to user_post_path(current_user.id, Post.find(params[:post_id]))
-  # end
-
   def post_params
     params.require(:post).permit(:title, :text)
   end
