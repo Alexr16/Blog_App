3c61428505cc2628d21e62cf9c695a73de4a7cb0
Alexr16 <j.alex-1996@hotmail.com>
2022-07-18 14:39:47 -0500

    Add render user show test

--- spec/views/user_index_page_spec.rb
+++ spec/views/user_index_page_spec.rb
@@ -6,14 +6,22 @@ RSpec.describe 'User index page', type: :system do
       @jorge = User.create(name: 'Jorge', photo: 'https://i.stack.imgur.com/YQu5k.png', bio: 'Hello world')
       visit root_path
     end
+
     it 'Shows the username of all other users' do
       expect(page).to have_content('Jorge')
     end
+
     it 'Shows the user photo of all other users' do
       expect(page).to have_css('img[src*="https://i.stack.imgur.com/YQu5k.png"]')
     end
+
     it 'Shows the user post counter of all other users' do
       expect(page).to have_content('Number of posts: 0')
+    end
+
+    it 'Redirects to the user show page when clicking on the username' do
+      click_link 'Jorge'
+      expect(current_path).to eq(user_path(@jorge))
     end
   end
 end
