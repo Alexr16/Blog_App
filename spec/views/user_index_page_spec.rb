0b7d80078a75fafbf43c7f6a00cd1736c0af6222
Alexr16 <j.alex-1996@hotmail.com>
2022-07-18 14:30:29 -0500

    Test if user index page displays the number of posts each user has written

--- spec/views/user_index_page_spec.rb
+++ spec/views/user_index_page_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe 'User index page', type: :system do
     it 'Shows the username of all other users' do
       expect(page).to have_content('Jorge')
     end
+    it 'Shows the user photo of all other users' do
+      expect(page).to have_css('img[src*="https://i.stack.imgur.com/YQu5k.png"]')
+    end
+    it 'Shows the user post counter of all other users' do
+      expect(page).to have_content('Number of posts: 0')
+    end
   end
 end
