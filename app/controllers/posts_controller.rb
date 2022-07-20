128c931952be92e2de67c2f87948ad9a519d368b
Alexr16 <j.alex-1996@hotmail.com>
2022-07-20 14:05:13 -0500

    Add post delete method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @user = User.find(@post.author_id)
+    @user.update(post_counter: @user.posts.count - 1)
+    @user.save
+    puts @user.inspect
+    @post.destroy
+    redirect_to user_posts_path(current_user)
+  end
+
   def post_params
     params.require(:post).permit(:title, :text)
   end
