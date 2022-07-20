c0e7e679bef677e232d7f0e4612ba55b84dc81c1
Alexr16 <j.alex-1996@hotmail.com>
2022-07-20 14:04:27 -0500

    Add authorizations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    if user.role? :admin
+    if user.role == 'admin'
       can :manage, :all
     else
       can :read, :all
-      can :manage, Post, user_id: user.id
-      can :manage, Comment, user_id: user.id
-
+      can :manage, Post, author_id: user.id
+      can :manage, Comment, author_id: user.id
     end
   end
 end
