4edbc61fbe9e0484b3bee07c96aecd1bc7904953
Chris Clothier <c.r.clothier@gmail.com>
2022-07-21 09:25:37 -0500

    Add API model methods

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -4,4 +4,22 @@ class Api::V1::CommentsController < ApplicationController
         @comments = @post.comments.includes(:author)
         render json: @comments
     end
+
+    def create
+      @user = current_user
+      @post = Post.find(params[:post_id])
+      @comment = @user.comments.new(comments_params)
+      @comment.author_id = @user.id
+      @comment.post_id = @post.id
+      
+      if @comment.save
+          render json: { status: 201, message: 'Comment created successfully!', content: { comment: @comment } }
+      else
+          render json: @comment.errors, status: :bad_request, message: 'Operation failed'
+      end
+    end
+
+    def comments_params
+      params.require(:comment).permit(:text)
+  end
 end
